0df2169d8e3dedcdc88c27747f335605c82ad0e4
Nivi-B <113551675+Nivi-B@users.noreply.github.com>
2024-12-08 17:04:02 -0500

    added images

--- gamescene.js
+++ gamescene.js
@@ -4,6 +4,8 @@ class gamescene extends Phaser.Scene{
     }
   
     create(){
+      this.add.image(400, 300, "background");
+      this.character = this.add.sprite(50, 285, "character_idle");
       this.add.text(20, 20, "Playing game", {font: "25px Arial", fill: "yellow"});
     }
   }
--- preload.js
+++ preload.js
@@ -2,6 +2,10 @@ class preload extends Phaser.Scene {
     constructor() {
       super("bootGame");
     }
+    preload(){
+      this.load.image("background", "assets/background.png");
+      this.load.image("character_idle", "assets/character_idle.png");
+    }
     create() {
       this.add.text(20, 20, "Loading game...");
       this.scene.start("playGame");
