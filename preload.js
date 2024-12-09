f6033ea71898d85baac19d30340565e23bf4ab54
Nivi-B <113551675+Nivi-B@users.noreply.github.com>
2024-12-08 22:19:34 -0500

    implemented character jump

--- preload.js
+++ preload.js
@@ -9,10 +9,12 @@ class preload extends Phaser.Scene {
       this.load.image("character_run0", "assets/character_run0.png");
       this.load.image("character_run1", "assets/character_run1.png");
       this.load.image("character_run2", "assets/character_run2.png");
-      
+      this.load.image("character_jump", "assets/character_jump.png");
+
       this.load.image("car", "assets/car.png");
       this.load.image("police_car", "assets/police_car.png");
       this.load.image("ambulance", "assets/ambulance.png");
+
       this.load.image("snowflake", "assets/snowflake.png");
       this.load.image("power_up", "assets/power_up.png");
 
